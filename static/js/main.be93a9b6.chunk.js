395180e8035154d337d5973744b63bc8505bbcb4
yalanskyyalexey <yalanskyyalexey@gmail.com>
2021-11-11 11:12:44 +0200

    Updates

---
+++ static/js/main.be93a9b6.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-04-movies"]=this["webpackJsonpgoit-react-hw-04-movies"]||[]).push([[0],{11:function(e,t,c){"use strict";c.r(t);var n=c(1),i=c.n(n),o=c(3),r=c.n(o),s=(c(8),c(9),c(0));var a=function(){return Object(s.jsx)("div",{className:"App",children:Object(s.jsx)("h1",{children:"hello"})})};r.a.render(Object(s.jsx)(i.a.StrictMode,{children:Object(s.jsx)(a,{})}),document.getElementById("root"))},8:function(e,t,c){},9:function(e,t,c){}},[[11,1,2]]]);
+//# sourceMappingURL=main.be93a9b6.chunk.js.map
